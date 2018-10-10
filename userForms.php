344b69e7c4fc8bb482b41aa334708c5c5d4950f7
apatwa17 <ahpandroid5@gmail.com>
2018-10-10 11:50:30 -0500

    working on new user

--- userForms.php
+++ userForms.php
@@ -1,20 +1,6 @@
 <!--Forms Applicable for registered users-->
 
-<!--Login Form-->
-<div class="container" id="loginForm">
-    <h1>Login</h1>
-    <form>
-        <div class="form-group">
-            <label for="username">Username: </label>
-            <input type="text" id="username" placeholder="User Name" class="form-control">
-        </div>
-        <div class="form-group">
-            <label for="password">Password: </label>
-            <input type="password" id="password" class="form-control">
-        </div>
-        <button type="submit" class="btn btn-primary">Submit</button>
-    </form>
-</div>
+
 <!--Add Event Form-->
 <div class="container" id="addEvent">
     <h1>New Event</h1>
