5a42feacc87db4bb6cf3af03170542d59b501bd1
apatwa17 <ahpandroid5@gmail.com>
2018-10-22 11:42:47 -0500

    wrote get Events backend. needs tags list, firstday of month and lastday of month

---
+++ getEvents.php
@@ -0,0 +1,83 @@
+<?php
+session_start();
+header("Content-Type: application/json"); // Since we are sending a JSON response here (not an HTML document), set theMIME Type to application/json
+
+//Because you are posting the data via fetch(), php has to retrieve it elsewhere.
+$json_str = file_get_contents('php://input');
+//This will store the data into an associative array
+$json_obj = json_decode($json_str, true);
+
+//Variables can be accessed as such:
+$tags = $json_obj['tags'];
+$firstDay = $json_obj['firstDay'];
+$lastDay = $json_obj['lastDay'];
+//This is equivalent to what you previously did with $_POST['username'] and $_POST['password']
+
+//check log-in status
+if(!isset($_SESSION['userID'])){
+echo json_encode(array(
+"error" => true,
+"eMessage" => "Not Logged In"
+));
+exit;
+}
+$userID = $_SESSION['userID'];
+
+//connect to database
+require("dataBaseAnees.php");
+
+//make tag ids arrray
+//and prepare query to get tag ids based on name
+$tag_ids = [];
+$stmt = $mysqli->prepare("select id from tags where name=?");
+
+//
+foreach($tags as $tag_name){
+    $stmt->bind_param('s', $tag_name);
+    $stmt->execute();
+    $stmt->bind_result($tag_id);
+    $stmt->fetch();
+    $tag_ids[] = $tag_id;
+}
+
+$stmt->close();
+
+//make array for event ids
+//and prepare select query
+$event_ids = [];
+$stmt = $mysqli->prepare("select event_id from tags_events where tag_id=?");
+
+//execute query and place event ids in array
+foreach($tag_ids as $key){
+$stmt->bind_param('d', $tags[$key]);
+$stmt->execute();
+$stmt->bind_result($event_id);
+$stmt->fetch();
+$event_ids[] = $event_id;
+}
+
+$stmt->close();
+//remove duplicates from array
+$event_ids = array_unique($event_ids);
+
+//select events from db
+$events = [];
+$stmt = $mysqli->prepare("select title, date, time from events where (owner_id=?) and (id=?) and (date between ? and ?)");
+foreach($event_ids as $event_id){
+    $stmt->bind_param('ddss', $userID, $event_id, $firstDay, $lastDay);
+    $stmt->execute();
+    $stmt->bind_result($title, $date, $time);
+    $stmt->fetch();
+    $events[] = array(
+        "title" => $title,
+        "date" => $date,
+        "time" => $time
+    );
+}
+
+$stmt->close();
+
+echo json_encode($events);
+exit;
+
+?>
